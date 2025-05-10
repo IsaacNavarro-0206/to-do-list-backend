7d075af5ba8726dcc2cf20437c362e3b5d404e1b
Isaac Navarro <isaacnavarro2000@gmail.com>
2025-05-10 17:16:21 -0500

    chore: actualizar .gitignore para incluir carpetas de AWS y modificar la función de salud para simplificar el manejo de eventos

--- src/functions/health.ts
+++ src/functions/health.ts
@@ -1,13 +1,11 @@
-import { APIGatewayProxyEvent } from 'aws-lambda';
-
-export const handler = async (event: APIGatewayProxyEvent) => {
+export const handler = async () => {
   return {
     statusCode: 200,
     headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
     },
-    body: JSON.stringify({ ok: true })
+    body: JSON.stringify({ ok: true }),
   };
-}; 
+};
